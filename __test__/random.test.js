3801d02e620abae46ccc1bb4bd3765368192d323
Franklyn Tavarez <kurbby.450@gmail.com>
2022-04-07 15:44:57 -0700

    all of it

---
+++ __test__/random.test.js
@@ -0,0 +1,6 @@
+const randomNumber = require('../../../Users/Franklyn Tavarez/Downloads/10.1/lib/random');
+
+test('generates a random number between 1 and 10', () => {
+  expect(randomNumber()).toBeGreaterThanOrEqual(1);
+  expect(randomNumber()).toBeLessThanOrEqual(10);
+});
